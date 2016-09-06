e17f0799d8e8518617e0af7a762fd4f6accce8bb
Dima Sergeev <djvu@inbox.ru>
2016-09-06 13:18:25 +0300

    lockrun 3

---
+++ deb/usr/local/include/help/core.h
@@ -0,0 +1,20 @@
+#ifndef LIB_CORE_H
+#define LIB_CORE_H
+
+#include <time.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#define OK          0
+#define ERROR       -1
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif
---
+++ deb/usr/local/include/help/logger.h
@@ -0,0 +1,63 @@
+#ifndef LIB_LOG_H
+#define LIB_LOG_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+#include "core.h"
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+enum log_level_e
+{
+    log_level_error = 0,
+    log_level_warn,
+    log_level_notice,
+    log_level_debug
+};
+
+#define LOG_TIME_STR_SIZE   sizeof("[YYYY.MM.DD HH:MM:SS] ")
+#define LOG_PID_STR_SIZE    sizeof("-18446744073709551615")
+#define LOG_STR_SIZE        2048
+
+#define log_error(log, args...)         log_put(log, log_level_error,  __FILE__, __LINE__, args)
+#define log_warn(log, args...)          log_put(log, log_level_warn,   __FILE__, __LINE__, args)
+#define log_notice(log, args...)        log_put(log, log_level_notice, __FILE__, __LINE__, args)
+
+#ifdef DEBUG_ON
+    #define log_debug(log, args...)     log_put(log, log_level_debug, __FILE__, __LINE__, args)
+#else
+    #define log_debug(log, args...)
+#endif
+
+typedef struct
+{
+    int         level;
+    time_t      ts;
+    char        ts_str[LOG_TIME_STR_SIZE];
+    char        pid_str[LOG_PID_STR_SIZE];
+    const char  *file;
+    FILE        *fd;
+} log_t;
+
+log_t      *log_new(const char *file, int level);
+void        log_rotate(log_t *log);
+void        log_set_pid(log_t *log, pid_t pid);
+void        log_set_str_level(log_t *log, char *level);
+void        log_put(log_t *log, enum log_level_e level, const char *file, int line, const char *format, ...);
+void        log_delete(log_t *log);
+
+int         setproctitle_init(log_t *log, char **argv);
+int         setproctitle(log_t *log, char *title, char *PROGRAM_NAME);
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif

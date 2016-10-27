6df129185b9f79c09bfe5994cc37de970be167a5
djvu <djvu@inbox.ru>
2016-10-27 16:31:10 +0300

    get_hex bug fix 2

--- src/config.c
+++ src/config.c
@@ -12,17 +12,16 @@
 static int config_load(config_t *config, char *file);
 
 config_t *
-config_new(log_t *log, char *file)
-{
-    config_t    *config;
-    pool_t      *pool;
+config_new(log_t *log, char *file) {
+    config_t *config;
+    pool_t *pool;
 
     pool = pool_new(CONFIG_POOL_SIZE, CONFIG_POOL_RMD_SIZE);
     if (!pool) {
         log_error(log, "create pool error");
         return NULL;
     }
-    config = pool_calloc(pool, sizeof(config_t));
+    config = pool_calloc(pool, sizeof (config_t));
     if (!config) {
         log_error(log, "pool calloc error");
         pool_delete(pool);
@@ -34,27 +33,25 @@ config_new(log_t *log, char *file)
         pool_delete(pool);
         return NULL;
     }
-    
+
     return config;
 }
 
 char *
-config_get_str(config_t *config, char *key, char *def)
-{
-    PWord_t     pw;
-    
+config_get_str(config_t *config, char *key, char *def) {
+    PWord_t pw;
+
     JSLG(pw, config->data, (u_char *) key);
-    
+
     return pw ? (char *) *pw : def;
 }
 
 int
-config_get_int(config_t *config, char *key, int def)
-{
-    int         rc;
-    char        *k;
-    PWord_t     pw;
-    
+config_get_int(config_t *config, char *key, int def) {
+    int rc;
+    char *k;
+    PWord_t pw;
+
     JSLG(pw, config->data, (u_char *) key);
     if (pw) {
         k = (char *) *pw;
@@ -67,65 +64,61 @@ config_get_int(config_t *config, char *key, int def)
             return rc;
         }
     }
-    
+
     return def;
 }
 
 int
-config_get_hex(config_t *config, char *key, int def)
-{
-    int         rc;
-    char        *k;
-    PWord_t     pw;
-    
+config_get_hex(config_t *config, char *key, int def) {
+    int rc;
+    char *k;
+    PWord_t pw;
+
     JSLG(pw, config->data, (u_char *) key);
-    
-    k = (char *) *pw;
-    
-    if (k == NULL) {
-        return def;
+
+    if (pw) {
+        k = (char *) *pw;
+
+        return strtol(k, (char **) NULL, 16);
     }
-    
-    return strtol(k, (char **)NULL, 16);
+
+    return def;
 }
 
 unsigned int
-config_get_uint(config_t *config, char *key, unsigned int def)
-{
-    unsigned int    rc;
-    char            *k;
-    PWord_t         pw;
-    
+config_get_uint(config_t *config, char *key, unsigned int def) {
+    unsigned int rc;
+    char *k;
+    PWord_t pw;
+
     JSLG(pw, config->data, (u_char *) key);
     if (pw) {
         k = (char *) *pw;
         atoui(rc, k);
         return rc;
     }
-    
+
     return def;
 }
 
 int
-config_get_bool(config_t *config, char *key, int def)
-{
-    char        *k;
-    PWord_t     pw;
+config_get_bool(config_t *config, char *key, int def) {
+    char *k;
+    PWord_t pw;
 
-    JSLG(pw, config->data, (u_char *) key);    
+    JSLG(pw, config->data, (u_char *) key);
     if (pw) {
         k = (char *) *pw;
         return k[0] == 'y' && k[1] == 'e' && k[2] == 's' && k[3] == '\0';
     }
-    
+
     return def;
 }
 
 void
-config_delete(config_t *config)
-{
-    int     rc;
-    
+config_delete(config_t *config) {
+    int rc;
+
     if (config) {
         if (config->data) {
             JSLFA(rc, config->data);
@@ -137,16 +130,15 @@ config_delete(config_t *config)
 }
 
 static int
-config_load(config_t *config, char *file)
-{
-    int             fd, line, n;
-    char            c, *p, *k, *src, *end, *pkey, *pvalue;
-    char            index[CONFIG_KEY_MAX_SIZE + 1];
-    char            key[CONFIG_KEY_MAX_SIZE + 1];
-    ssize_t         size;
-    struct stat     sb;
+config_load(config_t *config, char *file) {
+    int fd, line, n;
+    char c, *p, *k, *src, *end, *pkey, *pvalue;
+    char index[CONFIG_KEY_MAX_SIZE + 1];
+    char key[CONFIG_KEY_MAX_SIZE + 1];
+    ssize_t size;
+    struct stat sb;
     static const u_char achar[] = "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0" "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0" "\0\0\0\0\0\0\0\0\0\0\0\0\0\0.\0" "0123456789\0\0\0\0\0\0" "\0ABCDEFGHIJKLMNOPQRSTUVWXYZ\0\0\0\0_\0abcdefghijklmnopqrstuvwxyz\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0";
-    PWord_t         pw;
+    PWord_t pw;
 
     /*  открываем файл  */
     fd = open(file, O_RDONLY);
@@ -192,7 +184,7 @@ config_load(config_t *config, char *file)
     p = src;
     pkey = pvalue = NULL;
     end = src + sb.st_size;
-    
+
     while (p < end) {
         c = p[0];
         switch (state) {
@@ -343,7 +335,7 @@ config_load(config_t *config, char *file)
                             log_error(config->log, "pool_cpy error");
                             free(src);
                             return ERROR;
-                        }                        
+                        }
                         JSLN(pw, config->data, (u_char *) index);
                     }
                     state = space_s;

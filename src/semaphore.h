70cf1c5246031c9daba80f07cb2632165016e58c
djvu <djvu@inbox.ru>
2019-06-07 15:59:20 +0400

    semaphore add 4

--- src/semaphore.h
+++ src/semaphore.h
@@ -20,11 +20,29 @@ extern "C" {
 
 typedef struct semaphore semaphore_t;
 
-
+/**
+  * @brief Создать семафор.
+  */
 semaphore_t *semaphore_create(char *semaphore_name);
+
+/**
+  * @brief Открыть семафор.
+  */
 semaphore_t *semaphore_open(char *semaphore_name);
+
+/**
+  * @brief Освобождаем семафор.
+  */
 void semaphore_post(semaphore_t *semap);
+
+/**
+  * @brief Захватываем семафор.
+  */
 void semaphore_wait(semaphore_t *semap);
+
+/**
+  * @brief Закрыть семафор.
+  */
 void semaphore_close(semaphore_t *semap);
 
 
